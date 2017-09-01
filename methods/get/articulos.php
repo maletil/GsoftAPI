e42ef28a12c0cb741e0517a3b5d8d398a82a1262
Miguel Ángel <maletils@gmail.com>
2017-09-01 17:07:47 +0200

    Añadido query orden de SELECT en articulos

--- methods/get/articulos.php
+++ methods/get/articulos.php
@@ -17,19 +17,28 @@ $orderBy = "";
     if (isset($_GET["orderBy"])) {
         $orderBy = $_GET["orderBy"];
     }
+    if (isset($_GET["orderword"])) {
+        $orderword = $_GET["orderword"];
+    } else {
+        $orderword = "undefined";
+    }
 
     switch ($orderBy) {
         case "Nombre":
             $orderTable = "`articulos`.`Descripcion`";
+                if ($orderword == "undefined"){ $orderword = "ASC";}
         break;
         case "Familia":
             $orderTable = "SUBSTR(`articulos`.`Codigo`, 1, 2)";
+                if ($orderword == "undefined"){ $orderword = "ASC";}
         break;
         case "Fecha":
             $orderTable = "`Ultima Modificacion`";
+                if ($orderword == "undefined"){ $orderword = "DESC";}
         break;
         default:
             $orderTable = "`articulos`.`Descripcion`";
+            if ($orderword == "undefined"){ $orderword = "ASC";}
         break;
     }
 
@@ -46,9 +55,9 @@ $orderBy = "";
 
 
     if ($getPrice == "true") {
-        $output = "SELECT articulos.Contador, articulos.Codigo, Descripcion, `Precio Medio`, `Ultimo Precio` , `Ultima Modificacion` FROM articulos INNER JOIN articulos2 ON `articulos`.`Codigo` = `articulos2`.`Codigo` ". $whereField ." ORDER BY ". $orderTable ." ASC";
+        $output = "SELECT articulos.Contador, articulos.Codigo, Descripcion, `Precio Medio`, `Ultimo Precio` , `Ultima Modificacion` FROM articulos INNER JOIN articulos2 ON `articulos`.`Codigo` = `articulos2`.`Codigo` ". $whereField ." ORDER BY ". $orderTable . " ". $orderword;
     } else if ($getPrice == "false") {
-        $output = "SELECT Contador, Codigo, Nombre, Descripcion, `Ultima Modificacion` FROM articulos ". $whereField ." ORDER BY ". $orderTable ." ASC";
+        $output = "SELECT Contador, Codigo, Nombre, Descripcion, `Ultima Modificacion` FROM articulos ". $whereField ." ORDER BY ". $orderTable;
     }
     header('Content-Type: application/json');
     echo sqlRequest($output, $auth);
